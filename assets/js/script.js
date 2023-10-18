2a5deef71b1916270f1c98829503a0ce0ecf0dc2
Joelg96 <96305864+Joelg96@users.noreply.github.com>
2023-10-18 00:51:09 -0400

    Add contact form and email integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,31 +8,59 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **/
 
-// Add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
+function addLastUpdatedToFooter() {
+    const footer = document.getElementById("footer");
 
-window.onscroll = function () {
-    var top = window.scrollY;
-    if (top >= 100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
+    // Check if the "Last Updated" div doesn't already exist
+    if (!footer.querySelector(".last-updated")) {
+        // Create a new div element for the "Last Updated" information
+        const lastUpdatedDiv = document.createElement("div");
+        lastUpdatedDiv.className = "last-updated";
+        lastUpdatedDiv.textContent = "Last Updated: " + document.lastModified;
+
+        // Append the "Last Updated" div to the footer
+        footer.appendChild(lastUpdatedDiv);
     }
 }
 
-// Add current date to footer
+// Call the function to add "Last Updated" to the footer
+addLastUpdatedToFooter();
 
-// Get the footer element
-const footer = document.getElementById("footer");
+// Function for sending an email notifaction from contact form
+// Initialize Email.js outside of the function
 
-// Check if the "Last Updated" div doesn't already exist
-if (!footer.querySelector(".last-updated")) {
-    // Create a new div element for the "Last Updated" information
-    const lastUpdatedDiv = document.createElement("div");
-    lastUpdatedDiv.className = "last-updated";
-    lastUpdatedDiv.textContent = "Last Updated: " + document.lastModified;
 
-    // Append the "Last Updated" div to the footer
-    footer.appendChild(lastUpdatedDiv);
-}
+function sendMessage(event) {
+    emailjs.init("HQ2ZljQ_MCooUqhCw");  // Replace with your actual public key
+    event.preventDefault();  // Prevent the form from submitting the traditional way
+
+    const serviceID = "service_5j8rehu";  // Replace with your actual service ID
+    const templateID = "template_l5bj3sb";  // Replace with your actual template ID
+
+    // Create the params object with the form data
+    var params = {
+        sendername: document.querySelector("#name").value,
+        senderemail: document.querySelector("#email").value,
+        senderphone: document.querySelector("#number").value,
+        sendersubject: document.querySelector("#subject").value,
+        sendermessage: document.querySelector("#message").value,
+    };
+    
+    // Send the form data using Email.js
+    emailjs.send(serviceID, templateID, params)
+        .then(function (response) {
+            console.log("Email sent successfully:", response);
+            // Display a confirmation message to the user
+            alert('Thank you, ' + params['sendername'] + '! Your message has been sent. I should get back to you soon!');
+            
+            // Reset the form or perform other actions as needed
+            event.target.reset();  // Reset the form
+        })
+        .catch(function (error) {
+            console.error("Email sending failed:", error);
+            alert("OH NO! An error occurred! Your email could not be sent. Please try again or contact the email provided.");
+            // Handle errors or show an error message to the user
+        });
+// Attach the sendMessage function to the form submit event
+document.querySelector("#contact-form form").addEventListener('submit', sendMessage);
+    }
